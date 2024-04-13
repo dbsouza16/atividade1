dbf3d73984c95208ff54c6289e96ecf2fea9d476
Diego Barros <di.b@hotmail.com>
2024-04-13 17:29:33 -0300

    alterações 130424

--- gamemania/src/app/app.module.ts
+++ gamemania/src/app/app.module.ts
@@ -1,4 +1,4 @@
-import { NgModule } from '@angular/core';
+import { NgModule, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 /* Importações Angular Material*/
@@ -41,6 +41,7 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
+  schemas: [CUSTOM_ELEMENTS_SCHEMA]
 })
 export class AppModule { }
